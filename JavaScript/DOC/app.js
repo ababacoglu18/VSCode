3afccedc365fc65fe8c0062ab9d243751ed76ab5
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2023-01-29 17:19:47 +0300

    ElementsSelect

---
+++ JavaScript/DOC/app.js
@@ -0,0 +1,12 @@
+let value ;
+
+value = document;
+
+value = document.scripts.length;
+value = document.scripts[0];
+
+value = document.links.length;
+value = document.links[document.links.length-1].getAttribute("href");
+
+
+console.log(value);
