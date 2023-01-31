009cff36ec9fa3eb1a546296589cfefb834ce19b
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2023-01-31 20:49:17 +0300

    EventFiles.Added

---
+++ JavaScript/DOM EVENT/App.js
@@ -0,0 +1,7 @@
+const filterInput = document.getElementById("filter");
+
+console.log(filterInput); //Todo Arayın Kısmına tıklandığında focus durumuna geçiyor.
+
+
+filterInput.onfocus = function() {
+    console.log("Naber");}
