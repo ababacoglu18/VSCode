1d35a32e0d44f07883ad974a13efc4b0028414f4
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2023-02-16 16:29:21 +0300

    TodoLocalStoragePush.Add

--- TodoListProject/todo.js
+++ TodoListProject/todo.js
@@ -13,15 +13,68 @@ eventListeners();
 
 function eventListeners(){ //Tüm event listenerlar.
     form.addEventListener("submit", addTodo);
-    
+    document.addEventListener("DOMContentLoaded",loadAllTodosToUI);
 
+}
+function loadAllTodosToUI(){ 
+
+    let todos = getTodosFromStorage(); 
+    todos.forEach(function(todo){
+        addTodoToUI(todo); 
+    }    
+    );
 }
 function addTodo(e){
         const newTodo = todoInput.value.trim();
 
-        addTodoToUI(newTodo); 
+        if (newTodo === ""){
+            /*<div class="alert alert-danger" role="alert">
+  This is a danger alert—check it out!
+</div>*/
+            showAlert("danger","Lütfen Geçerli Bir Todo Girin");
+        }
+        else{
+            addTodoToUI(newTodo); 
+            addTodoToStorage(newTodo);
+
+            showAlert("success","Todo Başarıyla Eklendi.");
+            
+            
+        }
+
+        
         
     e.preventDefault(); // sayfa önceye yüklenmesin diye.
+}
+function addTodoToStorage(newTodo) {   ///ÖNEMLİ!
+    let todos = getTodosFromStorage();
+
+    todos.push(newTodo);
+
+    localStorage.setItem("todos",JSON.stringify(todos));  //1. Key 2. Value
+}
+
+function getTodosFromStorage(){  /// Storagedan bütün todoları alıyor. 
+    let todos;
+
+    if (localStorage.getItem("todos") === null){
+        todos = [];
+    
+    }
+    else{
+        todos = JSON.parse(localStorage.getItem("todos")); //JSON.parse string arraye çeviriyor. 
+    }
+    return todos;
+}
+function showAlert(type, message){   ///CREATE ALERT FUNCTION!
+    const alert  = document.createElement("div");
+    alert.className = `alert alert-${type}`;
+    alert.textContent = message;    
+
+    firstCardBody.appendChild(alert);
+    
+    setTimeout(function(){alert.remove()},3000);
+
 }
 
 function addTodoToUI(newTodo){// String değerini list item olarak ekleyecek. 
